1e37edfeee238f0b7ea08bbbc77097324ae069bc
Marcos Gutierrez <victordavidmarcos123@gmail.com>
2026-01-21 22:52:05 -0800

    Add Module 1 Python programs: hello world, sum of two ints, largest of three

---
+++ Module1_Python/sum_of_two_ints.py
@@ -0,0 +1,9 @@
+# Ask the user for two integers
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+# Calculate the sum
+sum_result = num1 + num2
+
+# Show the result
+print("The sum of", num1, "and", num2, "is", sum_result)
